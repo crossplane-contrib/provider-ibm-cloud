ffaaeb01d0997d18f9c86c8a5e468110298e438c
Harry Stavropoulos <hstavrop@us.ibm.com>
2022-02-10 13:07:47 -0500

    tmp checkin

--- pkg/controller/vpcv1/vpc.go
+++ pkg/controller/vpcv1/vpc.go
@@ -124,7 +124,7 @@ func (c *vpcExternal) Observe(ctx context.Context, mg resource.Managed) (managed
 		return managed.ExternalObservation{}, errors.New("ListVpcs returned status code: " + string(response.StatusCode) + ", and response: " + response.String())
 	} else if vpcCollection != nil {
 		for cloudVPC := range vpcCollection.Vpcs {
-
+			
 		}
 		crossplaneCluster.Status.AtProvider, err = crossplaneClient.GenerateCrossplaneClusterInfo(ibmClusterInfo)
 		if err != nil {
